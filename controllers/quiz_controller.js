8be391e6360fa38814375f2ade03ddaf6cb31c0b
julenr <isusk246@gmail.com>
2015-07-13 14:55:30 +1200

    Anadidas el layout

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,15 @@
 
 // GET /quizes/question
 exports.question = function(req, res) {
-    res.render('quizes/question', {pregunta: 'Capital de Italia'});
+    res.render('quizes/question', {pregunta: 'Capital de Italia', title: 'Quiz'});
 };
 
 // GET /quizes/answer
 exports.answers = function(req, res) {
     if(req.query.respuesta === 'Roma') {
-        res.render('quizes/answers', {respuesta: 'Correcto'});
+        res.render('quizes/answers', {respuesta: 'Correcto', title: 'Quiz'});
     } else {
-        res.render('quizes/answers', {respuesta: 'Incorrecto'});
+        res.render('quizes/answers', {respuesta: 'Incorrecto', title: 'Quiz'});
     }
 
 };
